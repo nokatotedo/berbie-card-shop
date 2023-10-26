5c6e4ff6b81a4fbfb9db40262689c3bd8a0f9aea
wedaiputu <wedaiputu@gmail.conm>
2023-10-26 13:50:52 +0530

    barnbie

---
+++ public/script/slider.js
@@ -0,0 +1,27 @@
+
+const slider = document.querySelector(".slider");
+    const prevButton = document.getElementById("prev");
+    const nextButton = document.getElementById("next");
+    
+    let index = 0;
+    
+    nextButton.addEventListener("click", () => {
+        index++;
+        if (index >= slider.children.length) {
+            index = 0;
+        }
+        updateSlider();
+    });
+    
+    prevButton.addEventListener("click", () => {
+        index--;
+        if (index < 0) {
+            index = slider.children.length - 1;
+        }
+        updateSlider();
+    });
+    
+    function updateSlider() {
+        const offset = -index * 500; // Assuming each image has a width of 500px
+        slider.style.transform = `translateX(${offset}px)`;
+    }
